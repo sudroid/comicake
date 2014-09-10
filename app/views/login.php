4cc29b7839dee536031aab8c9b30ca6f65ed5924
sudroid <sus.chan6@gmail.com>
2014-09-10 13:46:47 -0400

    second commit; same files
    minor changes to welcome and sign in page

---
+++ app/views/login.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Comic Center Home</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #fff;
+			background-color:#FF5258
+		}
+
+		.welcome {
+			width: 300px;
+			height: 200px;
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			margin-left: -150px;
+			margin-top: -100px;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+	</style>
+</head>
+<body>
+	<div class="welcome">
+	<h1>Sign In.</h1>
+		User Name: <input id="loginName"><br/>
+		Password: <input id="loginPassword">
+	</div>
+</body>
+</html>
